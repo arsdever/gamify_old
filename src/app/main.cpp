4a6c00fe02b3fa2f66486f1b15a58baf11ee414c
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-10-12 00:11:39 +0400

    Initial commit

---
+++ src/app/main.cpp
@@ -0,0 +1,11 @@
+#include <stdafx_qt>
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv);
+
+	QWidget window;
+	window.show();
+
+	return app.exec();
+}
---
+++ src/stdafx/stdafx_qt.cpp
@@ -0,0 +1,2 @@
+#include <stdafx>
+#include <stdafx_qt>
