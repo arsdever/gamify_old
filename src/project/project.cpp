54ff82b934e46dcbe8b6d539635d15c84d89e73f
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-10-15 21:15:42 +0400

    Provide mechanisms for project modification
    Add name and active scene setters
    Add signals for name and active scene change notification

--- src/project/project.cpp
+++ src/project/project.cpp
@@ -13,4 +13,16 @@ project::project(std::string_view name)
 {
 }
 
+void project::set_name(std::string_view name)
+{
+    _name = name;
+    signal_name_changed(name);
+}
+
+void project::set_active_scene(scene_ptr scene)
+{
+    _active_scene = scene;
+    signal_active_scene_changed(scene);
+}
+
 } // namespace g::project
--- src/project/project.hpp
+++ src/project/project.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/signals2.hpp>
+
 namespace g::project
 {
 
@@ -7,6 +9,14 @@ class project
 {
 public:
     project(std::string_view name);
+
+    void set_name(std::string_view name);
+    void set_active_scene(scene_ptr scene);
+
+#pragma region signals
+public:
+    boost::signals2::signal<void(std::string_view)> signal_name_changed;
+    boost::signals2::signal<void(scene_ptr)> signal_active_scene_changed;
 
 private:
     std::string _name;
