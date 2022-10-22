4399c901a8fefcb4902f488b9722ffebb6ef5444
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-10-22 22:08:32 +0400

    Make a dummy scene

--- src/view/main_window.cpp
+++ src/view/main_window.cpp
@@ -12,7 +12,7 @@ namespace g::view
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow { parent }
     , _project { std::make_shared<project::project>("Dummy project") }
-    , _scene { std::make_shared<project::scene>("Dummy scene") }
+    , _scene { project::scene::create("Dummy scene") }
     , _sceneWidget { new SceneView { _scene } }
     , _view { new QWidget }
 {
@@ -25,6 +25,16 @@ MainWindow::MainWindow(QWidget* parent)
     setMenuBar(new QMenuBar);
     QMenu* sceneMenu = menuBar()->addMenu("Scene");
     sceneMenu->addActions(_sceneWidget->actions());
+
+    _scene->add_child(project::object::create("Dummy object"))
+        ->add_child(project::object::create("Dummy child"));
+
+    _scene->add_child(project::object::create("Dummy object 2"))
+        ->add_child(project::object::create("Dummy child 2"));
+
+    _scene->add_child(project::object::create("Dummy object 3"))
+        ->add_child(project::object::create("Dummy child 3"))
+        ->add_child(project::object::create("Dummy child 3.1"));
 }
 
 } // namespace g::view
