f0e429729b0a77ff2a2586db5baa1fee941fc72b
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-10-30 00:17:10 +0400

    Update engine project
    Remove clock class
    Add engine class

--- src/engine/prototypes.hpp
+++ src/engine/prototypes.hpp
@@ -1,15 +1,15 @@
 
 #pragma once
 
-namespace g::project
+namespace g::engine
 {
 
-class clock;
-using clock_ptr = std::shared_ptr<clock>;
-using clock_wptr = std::weak_ptr<clock>;
-using clock_uptr = std::unique_ptr<clock>;
-using clock_cptr = std::shared_ptr<const clock>;
-using clock_cwptr = std::weak_ptr<const clock>;
-using clock_cuptr = std::unique_ptr<const clock>;
+class engine;
+using engine_ptr = std::shared_ptr<engine>;
+using engine_wptr = std::weak_ptr<engine>;
+using engine_uptr = std::unique_ptr<engine>;
+using engine_cptr = std::shared_ptr<const engine>;
+using engine_cwptr = std::weak_ptr<const engine>;
+using engine_cuptr = std::unique_ptr<const engine>;
 
 } // namespace g::project
