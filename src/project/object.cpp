3315127acb8333323a39aedac8dc8acc493bd1f2
Arsen <arsen.gharagyozyn.96@gmail.com>
2022-11-13 01:29:57 +0400

    Add object context menu in the scene view  (#2)
    * Add const object prototypes
    
    * Trigger signals when child is added or removed. Add unit test.
    
    * Update scene_model.hpp
    
    * Fix signal naming in the object class
    
    * Add get_ptr functions in object class
    
    * Add possibility to remove object from scene
    
    * Fix the scene model
    
    * Add new case in unit test and remove header.
    
    * Fix signals renaming
    
    * Remove new line
    
    * Make scene an object
    
    * Make a dummy scene
    
    * fixup! Make scene an object
    
    * Add a context menu in the scene view
    
    * Fix object adding/removing mechanism
    
    * Revert "Merge branch 'main' of github.com:arsdever/gamify into add-object-context-menu"
    
    This reverts commit 41df8c929ba96c07843e3fdd1d8069093ecbc6e0, reversing
    changes made to e32a33622124020526dacd4b1216deebdc2cbbdb.
    
    * Update settings.json to auto add final newline
    
    * Move dummy scene creation before creating the view
    
    * Fix macOS compilation
    
    Co-authored-by: liana-mazayan <liana.mazayan@picsart.com>
    Co-authored-by: lia-m99 <70721124+lia-m99@users.noreply.github.com>
    Co-authored-by: lia-m99 <mazayanlian@gmail.com>

--- src/project/object.cpp
+++ src/project/object.cpp
@@ -25,27 +25,35 @@ std::list<object_ptr> const& object::children() const { return _children; }
 
 object_ptr object::parent() const { return _parent.lock(); }
 
-void object::add_child(object_ptr obj)
+object_ptr object::add_child(object_ptr obj)
 {
-    if (std::find(_children.begin(), _children.end(), obj) == _children.end()) {
-        //As the parent is being changed this child should be removed from the old parent.
-        if (object_ptr parent = obj->_parent.lock()){
+    if (std::find(_children.begin(), _children.end(), obj) == _children.end())
+    {
+        // As the parent is being changed this child should be removed from the
+        // old parent.
+        if (object_ptr parent = obj->_parent.lock())
+        {
             parent->remove_child(obj);
         }
 
+        on_before_child_added(shared_from_this(), obj);
         obj->_parent = weak_from_this();
         _children.push_back(obj);
-        signal_children_list_changed();
-        obj->signal_parent_changed(obj->_parent.lock());
+        on_children_list_changed();
+        obj->on_parent_changed(obj->_parent.lock());
+        on_child_added(shared_from_this(), obj);
     }
+    return obj;
 }
 
 void object::remove_child(object_ptr obj)
 {
-    signal_children_list_changed();
-    obj->signal_parent_changed(shared_from_this());
+    on_before_child_removed(shared_from_this(), obj);
     _children.remove(obj);
     obj->_parent.reset();
+    on_children_list_changed();
+    obj->on_parent_changed(shared_from_this());
+    on_child_removed(shared_from_this(), obj);
 }
 
 void object::move(object_ptr parent)
@@ -59,5 +67,9 @@ void object::move(object_ptr parent)
     _parent = parent;
     parent->add_child(shared_from_this());
 }
+
+object_cptr object::get_ptr() const { return shared_from_this(); }
+
+object_ptr object::get_ptr() { return shared_from_this(); }
 
 } // namespace g::project
--- src/project/scene.cpp
+++ src/project/scene.cpp
@@ -7,21 +7,13 @@
 namespace g::project
 {
 
-scene::scene(std::string_view name)
-    : _name(name)
+scene::scene() = default;
+
+scene_ptr scene::create(std::string_view name)
 {
+    scene_ptr s { new scene {} };
+    s->_name = name;
+    return s;
 }
-
-std::string scene::name() const { return _name; }
-
-object_ptr scene::create_object(std::string_view name, object_ptr parent)
-{
-    object_ptr obj = object::create(name, parent);
-    _objects.push_back(obj);
-    on_object_added(obj);
-    return obj;
-}
-
-std::list<object_ptr> const& scene::objects() const { return _objects; }
 
 } // namespace g::project
